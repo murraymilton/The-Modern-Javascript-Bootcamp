977668d9730247cb35d415e89e36f7ce59bb8d75
murraymilton <murray.l.milton@outlook.com>
2023-03-27 17:49:22 +0200

    added an example of method chaining

---
+++ Section3_How_To_Model_Data_Efficiently/script.js
@@ -0,0 +1,17 @@
+"use strict";
+//Booleans
+let isOfAge = true;
+console.log(isOfAge);
+//Strings
+let userName = "User Name";
+console.log(userName.charAt(2));
+
+let currentSong = "Blame it On the Boogie";
+console.log(currentSong[6]);
+
+//String Methods
+console.log(currentSong.toUpperCase());
+
+// Demostrating Method Chaining
+let carName = "              MercedesAMG G-63               ";
+console.log(carName.trim().toUpperCase());
